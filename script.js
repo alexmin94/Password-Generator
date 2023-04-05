afb4e237981df86a963b42b5b779a0cde3c986fb
Alex <aminyakov94@gmail.com>
2023-04-05 09:15:15 -0500

    addl code script.js, variables declared, first prompt coded

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log( "generatePassword called because of click on generateBtn");
+
+  let intendedChars = '';
+  let genPassword = ''
+  let passLen = 0;
+  let lower,upper,num,special = false;
+  let index = 0;
+  let randomChar = '';
+
+  passLen = prompt("Enter passlength between 8 - 128");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
